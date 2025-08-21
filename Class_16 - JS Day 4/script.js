de2fb61f6df5c31bd9aff5adcc1f57aeebdcad9f
Sapnendra <sapnendrajaiswal2388@gmail.com>
2025-08-21 16:39:12 +0530

    add class 16 javascript content

---
+++ Class_16 - JS Day 4/script.js
@@ -0,0 +1,52 @@
+// var math = 60;
+// var eng = 75;
+// var com = 85;
+
+// var totalMarks = math + eng + com;
+// var averageMarks = totalMarks / 3;
+
+// if(averageMarks >= 33) {
+//     console.log("Congratulations! You passed the exam.");
+// } else {
+//     console.log("Sorry! You failed the exam.");
+// }
+
+
+// var a = 35;
+// var b = 40;
+
+// if(a === b) {
+//     console.log("It is a square.");
+// } else {
+//     console.log("It is not a square.");
+// }
+
+// var tempF = 102;
+// // var tempF = (tempC * 9/5) + 32;
+// console.log("Temperature in Fahrenheit: " + tempF);
+// var tempC = (tempF - 32) * 5/9;
+// console.log("Temperature in Celsius: " + tempC);
+
+// if(tempC > 35) {
+//     console.log("Don't got outside!");
+// } else {
+//     console.log("You can go outside.");
+// }
+
+var marks = 33;
+console.log("Your marks is: " + marks);
+if(marks >= 80) {
+    console.log("You got grade: A+");
+}
+else if(marks >= 70) {
+    console.log("You got grade: B+");
+}
+else if(marks >= 60) {
+    console.log("You got grade: C+");
+}
+else if(marks >= 33) {
+    console.log("You got grade: D+");
+}
+else {
+    console.log("You got grade: F+");
+}
