7eb831942926ee436a9545ec7185d5dbd3fc1cca
Sapnendra <sapnendrajaiswal2388@gmail.com>
2025-08-21 15:53:20 +0530

    Add New classes content

---
+++ Class_15 - JS Day-3/script.js
@@ -0,0 +1,23 @@
+// Displaying personal information
+console.log("Sapnendra");
+console.log("Age: 21");
+console.log("City: Bhopal");
+
+// Displaying education details
+var schoolName = "Indira Gandhi International School";
+console.log("School Name: " + schoolName);
+var whichClass = "Class 10th";
+console.log("Class: " + whichClass);
+var rollNo = 12345;
+console.log("Roll No: " + rollNo);
+
+// Displaying skills and experience
+alert("Welcome to JS Learning!")
+
+// Asking questions
+var favFood = prompt("Enter your favorite food:");
+console.log("Favorite Food: " + favFood);
+
+// Displaying result of the question
+var wantToLearn = confirm("Do you want to learn JavaScript?");
+console.log("Learning JavaScript: " + wantToLearn);
