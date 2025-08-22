aaa6f4e47abba2f161832b7aaae1292973b3e673
Sapnendra <sapnendrajaiswal2388@gmail.com>
2025-08-22 17:58:34 +0530

    js day 5 - data type conversion

---
+++ Class_17 - JS Day 5/script.js
@@ -0,0 +1,27 @@
+// var age = 13;
+
+// if(age >= 18) {
+//     console.log("You are eligible to visit Oyo.");
+// }
+// else {
+//     console.log("You can go oyo after " + (18 - age) + " years.");
+// }
+
+
+// var salary = 15000;
+// if(salary >= 10000 && salary <= 20000) {
+//     var raise = (15000 * 5)/100;
+//     console.log("Your new salary is: " + (salary + raise));
+// } else {
+//     console.log("You don't need to raise your salary.");
+// }
+
+
+// var salary = 15000;
+// if(salary >= 10000 && salary <= 20000) {
+//     var tax = (15000 * 5)/100;
+//     console.log("Your new salary is: " + (salary - tax));
+// } else {
+//     console.log("You don't need to pay tax.");
+// }
+
