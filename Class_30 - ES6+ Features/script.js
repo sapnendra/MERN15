0db78363c3fc07724babf23e95f27a7c28286b60
Sapnendra <sapnendrajaiswal2388@gmail.com>
2025-09-09 17:07:03 +0530

    Add ES6+ features class content

---
+++ Class_30 - ES6+ Features/script.js
@@ -0,0 +1,21 @@
+// let a = 10;
+// let fun = () => {
+//   let a = 20;
+//   console.log(a);
+// };
+// console.log(a);
+
+// fun();
+// console.log(a);
+
+// if (true) {
+//   let a;
+// }
+
+// function abs() {
+//   var a = 10;
+// }
+// abs();
+// console.log(a);
+
+
