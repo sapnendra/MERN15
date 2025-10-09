6af67c6d7eca4f33dd40771d77940f78d3c37e95
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-10-09 09:31:19 +0530

    add modified files

--- Class_44 - JS DOM interval-timeout/script.js
+++ Class_44 - JS DOM interval-timeout/script.js
@@ -1,21 +1,31 @@
 const h5 = document.querySelector("h5");
 const h3 = document.querySelector("h3");
-const inner = document.querySelector(".inner");
 const btn = document.querySelector("button");
-
-h5.style.display = "none";
+const inner = document.querySelector(".inner");
 
 btn.addEventListener("click", () => {
   btn.disabled = true;
-  let grow = 0;
-  let inter = setInterval(() => {
-    grow++;
-    h3.textContent = grow + "%";
-    inner.style.width = `${grow}%`;
-  }, 30);
-
+  let sec = 3;
+  h3.textContent = `Download starts in ...${sec}`;
+  let secInter = setInterval(() => {
+    sec--;
+    h3.textContent = `Download starts in ...${sec}`;
+  }, 1000);
   setTimeout(() => {
-    h5.style.display = "block";
-    clearInterval(inter);
+    clearInterval(secInter);
+    let grow = 0;
+    let inter = setInterval(() => {
+      grow++;
+      h3.textContent = "Downloading... " + grow + "%";
+      h3.style.color = "rgb(255, 166, 0)";
+      inner.style.width = `${grow}%`;
+    }, 50);
+
+    setTimeout(() => {
+      btn.disabled = false;
+      h3.textContent = "Download Completed";
+      h3.style.color = "green";
+      clearInterval(inter);
+    }, 5000);
   }, 3000);
 });
